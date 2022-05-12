469d270bbd91ef4e2bb1fed3c80dca03d630de91
Scott McKenzie <scott@noizyland.net>
2022-05-13 00:54:46 +1000

    Deploy functionapp package via Terraform

---
+++ terraform/deploy.tf
@@ -0,0 +1,36 @@
+data "archive_file" "app" {
+  type        = "zip"
+  source_dir  = "../src"
+  output_path = "function-app.zip"
+}
+
+resource "azurerm_storage_container" "app" {
+  name                  = "content"
+  storage_account_name  = azurerm_storage_account.app.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "app" {
+  name                    = "${filesha256(var.archive_file.output_path)}.zip"
+  storage_account_name    = azurerm_storage_account.app.name
+  storage_container_name  = azurerm_storage_container.app.name
+  type                    = "Block"
+  source                  = var.archive_file.output_path
+}
+
+data "azurerm_storage_account_blob_container_sas" "app" {
+  connection_string = azurerm_storage_account.app.primary_connection_string
+  container_name    = azurerm_storage_container.app.name
+
+  start = "2022-01-01T00:00:00Z"
+  expiry = "2032-01-01T00:00:00Z"
+
+  permissions {
+    read   = true
+    add    = false
+    create = false
+    write  = false
+    delete = false
+    list   = false
+  }
+}
