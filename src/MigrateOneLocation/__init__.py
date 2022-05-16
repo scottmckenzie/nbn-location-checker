7e90e04864d01cf2a9cdd30c7874d0c68099a4c2
Scott McKenzie <scott@noizyland.net>
2022-05-16 22:07:26 +1000

    Use nbn.get_location function

--- src/MigrateOneLocation/__init__.py
+++ src/MigrateOneLocation/__init__.py
@@ -1,13 +1,13 @@
 import azure.functions as func
 import logging
-from shared_code.nbn import get_location_from_nbn_api
+from shared_code.nbn import get_location
 
 
 def main(msg: func.QueueMessage, doc: func.Out[func.Document]) -> None:
     logging.info('Python queue trigger function processed a queue item: %s',
                  msg.get_body().decode('utf-8'))
     location_id = msg.get_body().decode('utf-8')
-    location = get_location_from_nbn_api(location_id)
+    location = get_location(location_id)
     if not location:
         return
     location.pop('timestamp')
