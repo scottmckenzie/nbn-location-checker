76697f84230f86f3a126a362a32e06d63decf68c
Scott McKenzie <scott@noizyland.net>
2022-05-16 22:06:53 +1000

    Add get_location function

--- src/shared_code/nbn.py
+++ src/shared_code/nbn.py
@@ -11,6 +11,18 @@ def get_location_and_store(location: str):
     return entity
 
 # get location from NBN API
+def get_location(location_id: str):
+    fn = '[get_location]'
+    url = f'https://places.nbnco.net.au/places/v2/details/{location}'
+    headers = {'referer': 'https://www.nbnco.com.au/'}
+    logging.info(f'{fn} requesting location {location_id}')
+    response = requests.get(url=url, headers=headers)
+    if not response.ok:
+        logging.error(f'{fn} request for {location_id} returned HTTP ' +
+            f'status {response.status_code}')
+        return None
+    return response.json()
+
 def get_location_from_nbn_api(location: str):
     fn = '[get_location_from_nbn_api]'
     url = f'https://places.nbnco.net.au/places/v2/details/{location}'
@@ -35,6 +47,18 @@ def get_nbn_status(code: str) -> str:
         return 'Good news! You may be able to upgrade to FTTP'
     logging.error(f'{fn} Unknown code {code}')
     return 'Unknown status code :('
+
+def get_location(location_id: str):
+    fn = '[get_location]'
+    url = f'https://places.nbnco.net.au/places/v2/details/{location}'
+    headers = {'referer': 'https://www.nbnco.com.au/'}
+    logging.info(f'{fn} requesting location {location_id}')
+    response = requests.get(url=url, headers=headers)
+    if not response.ok:
+        logging.error(f'{fn} request for {location_id} returned HTTP ' +
+            f'status {response.status_code}')
+        return None
+    return response.json()
 
 # store the location in the table
 def upsert_location(location):
