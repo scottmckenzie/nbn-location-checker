1f37fd72d5fe28818e1b4819678d794a75aa7084
Scott McKenzie <scott@noizyland.net>
2022-05-17 20:37:43 +1000

    Update cosmos db migration functions

--- src/MigrateAllSubscriptions/__init__.py
+++ src/MigrateAllSubscriptions/__init__.py
@@ -1,22 +1,27 @@
 import azure.functions as func
+import json
 import logging
 import typing
 from shared_code.table import AzureTableClient
 
 
 def main(msg: func.QueueMessage, msgout: func.Out[typing.List[str]]) -> None:
-    functionName = "'Function.MigrateAllLocations'"
+    functionName = "'Function.MigrateAllSubsciptions'"
     msg = msg.get_body().decode('utf-8')
     logging.info(f'{functionName} queue trigger function processed a ' +
                  f'queue item: {msg}')
     if not "start-migration" == msg:
         return
     messages = []
-    filter = f"RowKey eq '.'"
+    filter = f"RowKey ne '.'"
     with AzureTableClient.get() as table:
-        # iterate over all locations
-        for location in table.query_entities(filter):
-            messages.append(location['PartitionKey'])
+        # iterate over all subscriptions
+        for sub in table.query_entities(filter):
+            msg = {
+                "id": sub['PartitionKey'],
+                "email": sub['RowKey']
+            }
+            messages.append(json.dumps(msg))
     if messages:
         msgout.set(messages)
         logging.info(f'{functionName} Added {len(messages)} message(s) to the queue')
