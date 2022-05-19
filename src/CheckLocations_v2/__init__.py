84ce3b3e8530f950b733724904848a90bce176e8
Scott McKenzie <scott@noizyland.net>
2022-05-19 17:38:32 +1000

    Only check FTTC and FTTN locations. Fix email KeyError.

--- src/CheckLocations_v2/__init__.py
+++ src/CheckLocations_v2/__init__.py
@@ -1,10 +1,8 @@
-from webbrowser import get
 import azure.functions as func
 import json
 import logging
 import typing
-from shared_code.table import AzureTableClient
-from shared_code.cosmos import get_all_locations, get_location_subscribers
+import shared_code.cosmos as cosmos
 
 
 API_VERSION = 2
@@ -19,12 +17,15 @@ def main(mytimer: func.TimerRequest, msg: func.Out[typing.List[str]]) -> None:
     
     count = 0
     messages = []
-    for location in get_all_locations():
+    for location in cosmos.get_all_locations():
+        # ignore technologies not eligible for upgrade
+        if location['addressDetail']['techType'] not in ['FTTC','FTTN']:
+            continue
         count += 1
-        for subscriber in get_location_subscribers(location['id']):
+        for subscriber in cosmos.get_location_subscribers(location['id']):
             if not location.get('subscribers'):
                 location['subscribers'] = []
-            location['subscribers'].append(subscriber['RowKey'])
+            location['subscribers'].append(subscriber['email'])
         # add this location to the queue if it has subscribers
         if location.get('subscribers'):
             messages.append(json.dumps(location))
