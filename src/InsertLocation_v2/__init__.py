68e94be692fae7a0a173c00ac915e3cbf0dbff44
Scott McKenzie <scott@noizyland.net>
2022-05-19 13:26:57 +1000

    Fix functionName

--- src/InsertLocation_v2/__init__.py
+++ src/InsertLocation_v2/__init__.py
@@ -1,8 +1,11 @@
 import logging
 from shared_code.cosmos import upsert_location
 
+
+API_VERSION = 2
+
 def main(location: dict) -> bool:
-    functionName = "'Functions.InsertLoction'"
+    functionName = f"'Functions.InsertLoction_v{API_VERSION}'"
     logging.info(
         f'{functionName} activityTrigger inserting: {location["id"]}')
     upsert_location(location)
