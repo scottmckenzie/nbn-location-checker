0bf975e7d422c0c9f10beae9349474c67498fac9
Scott McKenzie <scott@noizyland.net>
2022-06-01 00:03:25 +1000

    Fix partition_key argument of delete_item.

--- src/shared_code/cosmos.py
+++ src/shared_code/cosmos.py
@@ -16,7 +16,7 @@ def create_subscription(subscription: dict) -> None:
     _m.subs.create_item(subscription)
 
 def delete_subscription(subscription: dict) -> None:
-    _m.subs.delete_item(subscription['id'], subscription['csa_id'])
+    _m.subs.delete_item(subscription['id'], subscription['id'])
 
 def get_all_locations() -> list:
     return list(_m.locations.read_all_items())
